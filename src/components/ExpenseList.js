f651997aefc773cbd06f92fd508ec66827e81348
Gourav <gore.auluck@gmail.com>
2021-03-19 20:52:00 -0700

    Added list items

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+
+const ExpenseList = () => {
+    const expenses = [
+        { id: 1231232, name: "Shopping", cost: 50},
+        { id: 1231232, name: "Holiday", cost: 300},
+        { id: 1231232, name: "Transportation", cost: 70},
+        { id: 1231232, name: "Child Care", cost: 500},
+        { id: 1231232, name: "Fuel", cost: 40},
+    ];
+
+    return (
+        <div>
+            <ul className="list-group">
+                {expenses.map(((expense)=>(
+                    <ExpenseItem 
+                        id={expense.id}
+                        name={expense.name}
+                        cost={expense.cost} />
+                )))}
+            </ul>      
+        </div>
+    )
+}
+
+
+
+export default ExpenseList
