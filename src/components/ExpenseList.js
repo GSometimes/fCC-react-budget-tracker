51884fa7d9aa28274e4231ba0f8914934b420448
Gourav <gore.auluck@gmail.com>
2021-03-20 21:20:00 -0700

    Added Context and Reducer

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,15 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ExpenseItem from './ExpenseItem'
+import { AppContext } from '../context/AppContext'
 
 const ExpenseList = () => {
-    const expenses = [
-        { id: 1231232, name: "Shopping", cost: 50},
-        { id: 1231232, name: "Holiday", cost: 300},
-        { id: 1231232, name: "Transportation", cost: 70},
-        { id: 1231232, name: "Child Care", cost: 500},
-        { id: 1231232, name: "Fuel", cost: 40},
-    ];
+    const { expenses } = useContext(AppContext);
 
+    
     return (
         <div>
             <ul className="list-group">
